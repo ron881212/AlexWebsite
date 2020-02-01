6718eaff5975762ff9aefb6df46adaead19baeac
Ronald Glover <ron881212@gmail.com>
2020-02-01 16:29:02 -0500

    Updates

---
+++ precache-manifest.798b5f55576da731d56279b24ab6d74b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50e1b57027653ea16213d20249be3c5",
+    "url": "/AlexWebsite/index.html"
+  },
+  {
+    "revision": "a0dfda1a32e945f2e992",
+    "url": "/AlexWebsite/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7fee11b85d5022c4309c",
+    "url": "/AlexWebsite/static/css/main.1da6b17f.chunk.css"
+  },
+  {
+    "revision": "a0dfda1a32e945f2e992",
+    "url": "/AlexWebsite/static/js/2.58790897.chunk.js"
+  },
+  {
+    "revision": "5ca59934c9ffe981782b111650307191",
+    "url": "/AlexWebsite/static/js/2.58790897.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fee11b85d5022c4309c",
+    "url": "/AlexWebsite/static/js/main.949b71e4.chunk.js"
+  },
+  {
+    "revision": "c99c06bd0208d5e4ec2d",
+    "url": "/AlexWebsite/static/js/runtime-main.02578803.js"
+  },
+  {
+    "revision": "789dbfa988f9bd787d301a444f6ae376",
+    "url": "/AlexWebsite/static/media/aboutPic.789dbfa9.jpeg"
+  },
+  {
+    "revision": "37ccdfebc5da7d990a4211dd89ed310b",
+    "url": "/AlexWebsite/static/media/areYouDown.37ccdfeb.jpeg"
+  }
+]);
