46f0181160a103451569f39755c6f740c2e73418
Ronald Glover <ron881212@gmail.com>
2020-02-06 04:30:40 -0500

    Changed about page styling

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,46 +7,30 @@ import { render } from '@testing-library/react'
 
 class About extends React.Component {
 
-    componentWillMount(){
-        document.body.style.backgroundColor = "#660850";
-    }
+    // componentWillMount(){
+    //     document.body.style.backgroundColor = "#660850";
+    // }
     
-    componentWillUnmount(){
-        document.body.style.backgroundColor = null;
-    }
+    // componentWillUnmount(){
+    //     document.body.style.backgroundColor = null;
+    // }
 
     render(){
 
     return(
         <>
         <Nav />
-        <div style={{
-            backgroundColor:'#660850',
-            display:'flex',
-            flexDirection:'row',
-            flexFlow: 'row wrap',
-            marginTop:80,
-            alignItems:'center',
-            justifyContent:'space-evenly'
-        }}>
-            <img 
-                className="img-fluid" 
-                style={{
-                    borderRadius:'50%',
-                    position:'relative', 
-                    display:'flex',
-                    // margin: 0,
-                    margin:10,
-                    height:500,
-                    width:500
-                }}
-                alt="Collectively-image" 
-                src={aboutme} />
-            <p 
-            className="mx-5" 
-            style={{display:'flex', fontSize:45, width:'40%',flexGrow:2,color:'#f4f4f4'}}>
+        <div className="jumbotron jumbotron-fluid text-center pb-5" style={{backgroundColor:'#001a30'}}>
+            <img src={aboutme} alt='code' style={{width:'350px'}} className="img-fluid rounded-circle mt-5"/>        
+        </div>
+        <div className="container">
+        <div className="card text-white mb-3 mx-auto" style={{backgroundColor:'#67054e'}}>
+            <div className="card-body">
+            <p className="card-text text-center" style={{fontSize:'25px'}}>
             My name is Alex and I love discussing crime and television! I make videos every week that review and analyze my favorite films and TV shows with a focus on women, scifi, LGBTQ, and the black community. I also post videos that discuss crime, culture, and vlog sometimes!
             </p>
+            </div>
+        </div>
         </div>
         </>
     )
